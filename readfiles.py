b135f7ae192adf599d93924bc6e90da6e8896825
Rajiv-Kinyanjui <peterkinyanjui226@gmail.com>
2022-05-25 17:24:52 +0300

    Try ecept method to check whether the name of the file is correct and the file can be found...and if not return a file not found error

--- readfiles.py
+++ readfiles.py
@@ -9,3 +9,17 @@ def read_file(text_file):
     with open(text_file, "r") as handle:
         data = handle.read()
         return data
+
+def read_file(text_file):
+    """
+    Function that reads a text file and returns the data from the text file
+
+    Raises:
+        FileNotFoundError:If it cannot find the file
+    """
+    try:
+        with open(text_file, "r") as handle:
+            data = handle.read()
+            return data
+    except FileNotFoundError:
+        return None
